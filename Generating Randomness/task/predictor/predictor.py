d26dae7c60c18e913c61f6bc880d3a77de2e2885
fvn70 <fvn_reg@mail.ru>
2022-03-11 16:47:21 +0700

    Stage 4/4: ”Generate randomness” game

--- Generating Randomness/task/predictor/predictor.py
+++ Generating Randomness/task/predictor/predictor.py
@@ -1,14 +1,20 @@
 import random
+import re
 
 
 def compare(s1, s2):
+    global sum
     cnt = 0
     num = len(s1)
     for i in range(num):
         if s1[i] == s2[i]:
             cnt += 1
+            sum -= 1
+        else:
+            sum += 1
     p = round(cnt / num * 100, 2)
     print(f'Computer guessed right {cnt} out of {num} symbols ({p} %)')
+    print(f'Your balance is now ${sum}\n')
 
 
 def predict(ss_in):
@@ -21,28 +27,52 @@ def predict(ss_in):
     return res
 
 
+def make_dic():
+    global dic
+    s = []
+    max = 100
+    while True:
+        print('Print a random string containing 0 or 1:\n')
+        s += [c for c in input() if c in '01']
+        ln = len(s)
+        if ln < max:
+            print(f'The current data length is {ln}, {max - ln} symbols left')
+        else:
+            break
+    ss = "".join(s)
+
+    print(f'\nFinal data string:\n{ss}\n')
+    quads = [ss[i:i+4] for i in range(len(ss))][:-3]
+    dic = {t: (quads.count(t + '0'), quads.count(t + '1')) for t in triads}
+
+
 triads = ["000", "001", "010", "011", "100", "101", "110", "111"]
-s = []
-max = 100
-while True:
-    print('Print a random string containing 0 or 1:\n')
-    s += [c for c in input() if c in '01']
-    ln = len(s)
-    if ln < max:
-        print(f'Current data length is {ln}, {max - ln} symbols left')
-    else:
-        break
-ss = "".join(s)
+dic = {}
+sum = 1000
 
-# ss = '010100101001010101111110001001011000010101010101010010101010101001010010101010001111001010010101010010101010101'
 
-print(f'\nFinal data string:\n{ss}\n')
-quads = [ss[i:i+4] for i in range(len(ss))][:-3]
-dic = {t: (quads.count(t + '0'), quads.count(t + '1')) for t in triads}
-# for k, v in dic.items():
-#     print(f'{k}: {v[0]},{v[1]}')
+def main():
+    print('''
+    Please give AI some data to learn...
+    The current data length is 0, 100 symbols left''')
+    make_dic()
+    print('''
+    You have $1000. Every time the system successfully predicts your next press, you lose $1.
+    Otherwise, you earn $1. Print "enough" to leave the game. Let's go!
+    ''')
 
-ss_in = input('Please enter a test string containing 0 or 1:\n')
-ss_out = predict(ss_in)
-print(f'prediction:\n{ss_out}\n')
-compare(ss_in[3:], ss_out[3:])
+    while True:
+        ss_in = input('Print a random string containing 0 or 1:\n')
+        if ss_in == 'enough':
+            break
+        if not re.match('[01]+', ss_in):
+            continue
+        ss_out = predict(ss_in)
+        print(f'prediction:\n{ss_out}\n')
+        compare(ss_in[3:], ss_out[3:])
+
+    print('Game over!')
+
+
+if __name__ == '__main__':
+    main()
